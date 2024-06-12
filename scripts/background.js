0bffb3edb7a28ad19ae8d43baae52d23992614e1
arthurfms <66703123+arthurfms@users.noreply.github.com>
2024-06-12 14:07:21 -0300

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,83 +1,17 @@
-const getOptions = () => {
-  chrome.storage.sync.get(
-    {
-      menucontext: true,
-      extension: true,
-      merContext: true,
-      itpContext: true,
-      affContext: true,
-      datafeed: true,
-      sasUI: true,
-      decoder: true,
-      ftpCred: true,
-      getMerchant: true,
-      testMerchant: true,
-      itp: true,
-      getAffiliate: true,
-      testAffiliate: true,
-    },
-    (items) => {
-      if (
-        items.menucontext &&
-        (items.merContext ||
-          items.itpContext ||
-          items.testLinkContext ||
-          items.affContext)
-      ) {
-        let contextOptions = {};
-        items.merContext
-          ? (contextOptions.merchant = "Go to Merchant: %s")
-          : "";
-        items.itpContext ? (contextOptions.itp = "Merchant ITP: %s") : "";
+chrome.contextMenus.create({
+  id: "merchant",
+  title: "Go to Merchant: %s",
+  type: "normal",
+  contexts: ["selection"],
+}, () => chrome.runtime.lastError);
 
-        items.affContext
-          ? (contextOptions.affiliate = "Go to Affiliate: %s")
-          : "";
-        // Add a listener to create the initial context menu items,
-        // context menu items only need to be created at runtime.onInstalled
-        for (const [op, title] of Object.entries(contextOptions)) {
-          chrome.contextMenus.create({
-            id: op,
-            title: title,
-            type: "normal",
-            contexts: ["selection"],
-          });
-        }
-
-        // Open a new search tab when the user clicks a context menu
-        chrome.contextMenus.onClicked.addListener((item, tab) => {
-          let opId = item.menuItemId;
-          let url;
-          let validatedUrl;
-
-          switch (opId) {
-            case "merchant":
-              validatedUrl =
-                /^\d+$/.test(item.selectionText.trim()) &&
-                parseInt(item.selectionText.trim()) > 95
-                  ? `https://account.shareasale.com/admin/adminDetailsMerchant.cfm?merchantId=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
-                  : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=merchants`;
-              url = new URL(validatedUrl);
-              break;
-            case "itp":
-              validatedUrl = `https://account.shareasale.com/admin/itp.cfm?merchantid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`;
-              url = new URL(validatedUrl);
-              break;
-            case "affiliate":
-              validatedUrl =
-                /^\d+$/.test(item.selectionText.trim()) &&
-                parseInt(item.selectionText.trim()) > 24
-                  ? `https://account.shareasale.com/admin/adminDetailsAffiliate.cfm?userid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
-                  : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=users`;
-              url = new URL(validatedUrl);
-              break;
-          }
-
-          chrome.tabs.create({ url: url.href, index: tab.index + 1 });
-        });
-      }
-    } // end items
-  );
-};
-
-getOptions();
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  let validatedUrl =
+        /^\d+$/.test(item.selectionText.trim()) &&
+        parseInt(item.selectionText.trim()) > 95
+          ? `https://account.shareasale.com/admin/adminDetailsMerchant.cfm?merchantId=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
+          : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=merchants`;
+  let url = new URL(validatedUrl);
+        
+  chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+});
